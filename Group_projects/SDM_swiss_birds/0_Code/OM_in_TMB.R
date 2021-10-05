8c9fb6c5967429779f1c9a23615203726f33192b
Gesa von Hirschheydt <gesa.vonhirschheydt@wsl.ch>
2021-10-05 15:36:25 +0200

    TMB-birds: expand OM to include covariates of occupancy and detection

--- Group_projects/SDM_swiss_birds/0_Code/OM_in_TMB.R
+++ Group_projects/SDM_swiss_birds/0_Code/OM_in_TMB.R
@@ -8,10 +8,10 @@
 # This code needs to be adjusted
 
 # set working directory
-setwd("H:/GitHub/Git_repositories_to_pull_from/Bayesian_thinking_UZH/Group_projects/SDM_swiss_birds/0_Code")
+setwd("C:/Users/Gesa/Documents/GitHub/GitHub_cloned_repositories_to_pull_from/Bayesian_thinking_UZH/Group_projects/SDM_swiss_birds/0_Code/")
 
 # define path to data folder
-folder <- "H:/GitHub/Git_repositories_to_pull_from/Bayesian_thinking_UZH/Group_projects/SDM_swiss_birds/1_Data/"
+folder <- "C:/Users/Gesa/Documents/GitHub/GitHub_cloned_repositories_to_pull_from/Bayesian_thinking_UZH/Group_projects/SDM_swiss_birds/1_Data/"
 
 
 
@@ -19,11 +19,13 @@ folder <- "H:/GitHub/Git_repositories_to_pull_from/Bayesian_thinking_UZH/Group_p
 #----------------#
 
 kite <- read.table(paste(folder,"kite.csv",sep=""), header=TRUE, sep =";")
+sites <- read.table(paste(folder,"sites.csv",sep=""), header=TRUE, sep =";")
 
 
 # load packages
 library(TMB)
 library(tidyverse)  # for summarise() and group_by() function
+library(robustHD) # for standardize()
 
 
 
@@ -31,33 +33,39 @@ library(tidyverse)  # for summarise() and group_by() function
 #-----------------------------------#
 
 ## Compile and load the model
-dyn.unload(dynlib("occupancy"))
+# dyn.unload(dynlib("occupancy"))
 compile("occupancy.cpp")
 dyn.load(dynlib("occupancy"))
 
 ## Data and parameters
 nvisits <- summarise(group_by(kite, site), visit=max(visit, na.rm=T))$visit
-cumdet <- summarise(group_by(kite, site), zst=max(det, na.rm=T))$zst
+cumdet <- summarise(group_by(kite, site), zst=max(det, na.rm=T))$zst # cumulative detection
+
+elev.st <- standardize(sites$elevation)
+# elev.st <- (sites$elevation-mean(sites$elevation))/sd(sites$elevation)
+X <- as.matrix(cbind(rep(1,nrow(sites)), elev.st, sites$farmland)); dim(X); head(X)
+
+time.st <- standardize(kite$time)
+Z <- as.matrix(cbind(rep(1,nrow(kite)), time.st)); dim(Z); head(Z)
 
 str(data <- list(y=kite$det,
              J=nvisits,
-             # knownOcc=cumdet,
-             nd=(-1)*(cumdet-1)   # nd is now exactly the opposite (1/0 reversed) of knownOcc
-             ))
-str(parameters <- list(logit_psi=0,
-                       logit_p=0))
-# str(parameters <- list(logit_psi=rep(0,times=length(nvisits)),
-#                        logit_p=rep(0,times=sum(nvisits))))
+             X=X,
+             Z=Z,
+             nd=(-1)*(cumdet-1))) # nd is now exactly the opposite (1/0 reversed) of cumdet
+(parameters <- list(beta=rep(0,ncol(X)),
+                    alpha=rep(0, ncol(Z))))
 
 ## Make a function object
 obj <- MakeADFun(data, parameters, DLL="occupancy")
 
 ## Call function minimizer
-opt <- nlminb(obj$par, obj$fn, obj$gr)
-opt
+(opt <- nlminb(obj$par, obj$fn, obj$gr))
 
 ## Get parameter uncertainties and convergence diagnostics
-sdr <- sdreport(obj) # this seems to never end...
-sdr
+(sdr <- sdreport(obj))
+summary(sdr, "fixed", p.value=TRUE)
+(AIC <- 2*opt$objective + 2*length(opt$par))
+(BIC <- 2*opt$objective + log(nrow(kite))*length(opt$par))
+
 (report <- obj$report())
-report
--- Group_projects/SDM_swiss_birds/0_Code/occupancy.cpp
+++ Group_projects/SDM_swiss_birds/0_Code/occupancy.cpp
@@ -9,18 +9,21 @@ template<class Type>
     //DATA_IVECTOR(knownOcc); //length: n.sites, 1 if a site has a detection? or is known to be occupied?
     DATA_IVECTOR(nd); //length = n.sites, nd=0: at least one detection at site; nd=1: no detection at site?
     DATA_IVECTOR(J); //Number of reps per site length: n.sites
-	DATA_MATRIX(X); //matrix of covariates
-    
+	DATA_MATRIX(X); //matrix of site covariates
+    DATA_MATRIX(Z); //matrix of visit covariates
+
     //PARAMETER_VECTOR(logit_psi(i)); //length = n. sites   WHY does this have a length of nsites?
     //PARAMETER_VECTOR(logit_p); //length = n.sites x n.reps  WHY does this have a length of nsites*nvisits
     //PARAMETER(logit_psi(i)); // length=1
-	PARAMETER(logit_p); // length=1
+	//PARAMETER(logit_p); // length=1
 	PARAMETER_VECTOR(beta); //parameter vector of length #covariates + 1
+	PARAMETER_VECTOR(alpha);
 	vector<Type> Xbeta = X*beta;
-
+	vector<Type> Zalpha = Z*alpha;
 	
-    vector<Type> psi(i) = invlogit(Xbeta);
-    Type p = invlogit(logit_p);
+    vector<Type> psi = invlogit(Xbeta);
+	vector<Type> p = invlogit(Zalpha);
+    //Type p = invlogit(logit_p);
     
     Type nll = 0;
     //int N = knownOcc.size(); //N=number of sites
@@ -30,7 +33,7 @@ template<class Type>
     for(int i=0; i<N; i++){
       Type cp = 1.0; // cp is a derived variable/help variable to calculate p
       for(int j=0; j<J(i); j++){
-        cp *= pow(p, y(k)) * pow(1-p, 1-y(k)); // What does this do?
+        cp *= pow(p(k), y(k)) * pow(1-p(k), 1-y(k)); // What does this do?
 		 //     when y(k)=1       when y(k)=0
         k++;
       }
@@ -47,7 +50,8 @@ template<class Type>
     }
 	
 	Type mean_psi = psi.sum()/N;
-    REPORT(p);
+    Type mean_p = p.sum()/y.size();
+	REPORT(mean_p);
 	REPORT(mean_psi);
     return nll;
   }
